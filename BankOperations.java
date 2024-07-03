8cf369028d6e4bf0c3a60a4c88ca20bc6dbd969b
Byreddy Jeevanreddy <byreddyjeevanreddy21@gmail.com>
2024-07-03 17:28:39 +0530

    add BankOperations class

--- BankOperations.java
+++ BankOperations.java
@@ -1,7 +1,7 @@
-public class interface BankOperations {void openAccount(String accountNumber, String accountHolderName, double initialBalance);
+public interface BankOperations {
+    void openAccount(BankAccount account);
     void closeAccount(String accountNumber);
     void deposit(String accountNumber, double amount);
     void withdraw(String accountNumber, double amount);
     void displayAccounts();
-
 }
